3fc974053f0cfc2eaf12be2bee002bcee73c9d1d
Nomlees <nomlees@bk.ru>
2017-12-18 19:52:33 +0300

    Метод download разделён на два метода: формирование текстового файла и запись ссылки в файл

--- src/ru/zak/music/Main.java
+++ src/ru/zak/music/Main.java
@@ -18,31 +18,41 @@ public class Main {
     }
 
     /**
-     * Метод формирует текстовый файл содержаий готовые ссылки, по шаблону регулярного выражения
+     * Метод формирует текстовый файл содержаий HTML-код.
+     * @return возвращает строку с HTML-кодом
      */
-    public static void Download(){
+    static String link() {
         String Url;
-        String result;
-        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT));
-             BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))) {
+        String result = null;
+        try (BufferedReader inFile = new BufferedReader(new FileReader(IN_FILE_TXT))) {
             while ((Url = inFile.readLine()) != null) {
                 URL url = new URL(Url);
                 try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
                     result = bufferedReader.lines().collect(Collectors.joining("\n"));
                 }
-                Pattern email_pattern = Pattern.compile("\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")");
-                Matcher matcher = email_pattern.matcher(result);
-                int i = 0;
-                while (matcher.find() && i < 2) {
-                    outFile.write(matcher.group() + "\r\n");
-                    i++;
-                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return result;
     }
 
+    /**
+     * Метод находит ссылки на скачивание музыки и записывает в outFile
+     */
+
+    static void result() {
+        try (BufferedWriter outFile = new BufferedWriter(new FileWriter(OUT_FILE_TXT))){
+            Pattern email_pattern = Pattern.compile("\\s*(?<=data-url\\s?=\\s?\")[^>]*\\/*(?=\")");
+            Matcher matcher = email_pattern.matcher(link());
+            int i = 0;
+            while (matcher.find() && i < 4) {
+                outFile.write(matcher.group() + "\r\n");
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
